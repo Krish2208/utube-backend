2c4ff0d4cd87d74d08cf8e2f945f7fb4270aa772
Krish Agrawal <diwankrish.17@gmail.com>
2022-05-29 20:20:32 +0530

    adds cors

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from utils import getChannelID, getVideoList, GetComment, GetStats, GetSentiment
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/videos", methods=['GET'])
 def getVideos():
